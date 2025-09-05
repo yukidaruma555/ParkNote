fe7c5b5e77b1cfe0c1dd1b5721966183356f91ce
EC2 Default User <211435376+yukidaruma555@users.noreply.github.com>
2025-09-05 06:43:13 +0000

    [Add] レイアウト

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -19,6 +19,7 @@ class ParksController < ApplicationController
     @park = Park.find(params[:id])
     @post = Post.new
     @posts = @park.posts.order(created_at: :desc)
+    @user = @park.user
   end
 
   def edit
@@ -29,6 +30,12 @@ class ParksController < ApplicationController
     park = Park.find(params[:id])
     park.update(park_params)
     redirect_to park_path(park.id)
+  end
+
+  def destroy
+    park = Park.find(params[:id])
+    park.destroy
+    redirect_to parks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :parks, only: [:new, :create, :index, :show, :edit, :update]
+  resources :parks, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update]
 
