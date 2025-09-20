43565c58d56843e80da0f48fa78eff5067602581
EC2 Default User <211435376+yukidaruma555@users.noreply.github.com>
2025-09-20 13:48:02 +0000

    [Add] バリデーションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,9 +27,12 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update(post_params)
-    redirect_to post_path(post.id)
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+    redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :parks, dependent: :destroy
   has_many :posts, dependent: :destroy
+
+  validates :name, presence: true
   
 
   def get_profile_image(width, height)
